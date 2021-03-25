7dacd7431cca468e4681179f64392c29c0f649b1
sem2436 <sem2436@naver.com>
2021-03-25 10:20:26 +0900

    java0325edu

---
+++ java0325_class.java
@@ -0,0 +1,23 @@
+package java0325;
+	
+public class java0325_class {
+	// 문제 : 아래가 실행되도록 해주세요.
+	public static void main(String[] args) {
+		사람 a사람 = new 사람();
+		
+		a사람.나이 = 20;
+		a사람.a왼팔 = new 팔();
+	}
+}
+
+
+
+class 사람 {
+	// 클래스안에 변수를 정의 할 수 있다.
+	int 나이;
+	팔 a왼팔;
+}
+
+class 팔 {
+	
+}
